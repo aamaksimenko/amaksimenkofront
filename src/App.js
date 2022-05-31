6c1ff982eb51d128426f9cab317696bb55e20a14
a-maksimenko <a.maksimenko@dunice.net>
2022-05-31 09:46:55 +0300

    start setting react redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <div>Home page</div>;
+};
 
 export default App;
