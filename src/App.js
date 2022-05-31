184df91ca6619e9f87db2f785c2ad4a345c242cf
a-maksimenko <a.maksimenko@dunice.net>
2022-05-31 10:35:29 +0300

    start main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const App = () => {
-  return <div>Home page</div>;
-};
+function App() {
+  return (
+    <header class="header">
+      <div class="header__container">
+        <span>News</span>
+        <a href="/">Home page</a>
+      </div>
+    </header>
+  );
+}
 
 export default App;
