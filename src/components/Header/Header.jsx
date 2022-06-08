e5a3993e5733d21d9b95161467ba3183a7f36a25
a-maksimenko <a.maksimenko@dunice.net>
2022-06-08 11:54:03 +0300

    add style for modal window

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React, { memo, useState } from 'react';
+import {
+  Box, TextField, Button,
+} from '@mui/material/';
 
 import Modal from '../Modal/Modal';
-
 import './index.css';
 
 function Header() {
-  const [modalActive, setModalActive] = useState(true);
+  const [modalActiveIn, setModalActiveIn] = useState(false);
+  const [modalActiveReg, setModalActiveReg] = useState(false);
+
   return (
     <header className="header">
       <div className="header__container">
@@ -17,21 +21,71 @@ function Header() {
           <input type="search" className="header__input" />
         </div>
         <nav className="header__menu">
-          <button type="button" onClick={() => setModalActive(true)}>Sign in</button>
-          <button type="button">Registration</button>
+          <button id="sign-in" type="button" onClick={() => setModalActiveIn(true)}>Sign in</button>
+          <button id="out" type="button" onClick={() => setModalActiveReg(true)}>Registration</button>
         </nav>
       </div>
-      <Modal active={modalActive} setActive={setModalActive}>
-        <form action="">
-          <p>First input</p>
-          <input type="text" />
-          <p>Second input</p>
-          <input type="text" />
-          <p>Third input</p>
-          <input type="text" />
-          <br />
-          <button type="button" className="submit">sign in</button>
-        </form>
+      <Modal active={modalActiveIn} setActive={setModalActiveIn}>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <h2>Sign in</h2>
+          <div className="reg-form">
+
+            <div className="reg-form__field">
+              <p>Email</p>
+              <TextField fullWidth required id="email" label="Email" />
+            </div>
+
+            <div className="reg-form__field">
+              <p>Password</p>
+              <TextField fullWidth required id="password" label="Password" type="password" autoComplete="current-password" />
+            </div>
+            <div className="reg-form__field">
+              <div id="in"><Button>Sign in</Button></div>
+              <div id="cls"><Button onClick={() => setModalActiveIn(false)}>Close</Button></div>
+            </div>
+          </div>
+        </Box>
+      </Modal>
+      <Modal active={modalActiveReg} setActive={setModalActiveReg}>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <h2>Registration form</h2>
+          <div className="reg-form">
+            <div className="reg-form__field">
+              <p>Name</p>
+              <TextField fullWidth required id="name" label="Name" />
+            </div>
+            <div className="reg-form__field">
+              <p>Email</p>
+              <TextField fullWidth required id="email" label="Email" />
+            </div>
+            <div className="reg-form__field">
+              <p>Login</p>
+              <TextField fullWidth required id="login" label="Login" />
+            </div>
+            <div className="reg-form__field">
+              <p>Password</p>
+              <TextField fullWidth required id="password" label="Password" type="password" autoComplete="current-password" />
+            </div>
+            <div className="reg-form__field">
+              <div id="reg"><Button>Registration</Button></div>
+              <div id="cls"><Button onClick={() => setModalActiveReg(false)}>Close</Button></div>
+            </div>
+          </div>
+        </Box>
       </Modal>
     </header>
   );
